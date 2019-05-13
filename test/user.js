6b47db86865e765a1a7d4fcec7708501f05ce773
Daniel Pomfret <daniel.pomfret@dwp.gsi.gov.uk>
2019-05-13 13:10:58 +0100

    feat(*): unit test framework

---
+++ test/user.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect;
+var user = require('../user');
+
+var log = require('../utils/log.js');
+global.log = log;
+
+var users = {};
+global.users = users;
+
+describe('user.connect()', function () {
+  it('should append the users array with new user', function () {
+
+    // 1. ARRANGE
+    var socket = { id: "test" };
+
+    // 2. ACT
+    user.connect(socket);
+
+    // 3. ASSERT
+    expect(users).to.have.keys([socket.id]);
+
+  });
+});
+
+describe('user.disconnect()', function () {
+  it('should remove socket from users array', function () {
+
+    // 1. ARRANGE
+    var socket = { id: "test" };
+
+    // 2. ACT
+    user.disconnect(socket);
+
+    // 3. ASSERT
+    expect(users).to.not.have.keys([socket.id]);
+
+  });
+});
