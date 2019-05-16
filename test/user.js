e1cb7c90ae058a66d28dbc4d558b4857dc64f3f7
Daniel Pomfret <daniel.pomfret@dwp.gsi.gov.uk>
2019-05-16 15:13:12 +0100

    fix(*): remove redunent tests

--- test/user.js
+++ test/user.js
@@ -7,25 +7,3 @@ global.log = log;
 var users = {};
 global.users = users;
 
-describe('user.connect()', function () {
-  it('should append the users array with new user', function () {
-
-    // 1. ARRANGE
-    var socket = { id: "test" };
-
-    // 2. ACT
-    user.connect(socket);
-
-    // 3. ASSERT
-    expect(users).to.have.keys([socket.id]);
-
-  });
-});
-
-describe('user.disconnect()', function () {
-  it('should remove socket from users array', function () {
-    var socket = { id: "test" };
-    user.disconnect(socket);
-    expect(users).to.not.have.keys([socket.id]);
-  });
-});
